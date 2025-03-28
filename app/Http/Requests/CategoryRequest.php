8d5067f963acab1afa764868b4aeab86b0e04330
renggamaulana <renggaa21@gmail.com>
2025-03-29 06:25:49 +0700

    update

--- app/Http/Requests/CategoryRequest.php
+++ app/Http/Requests/CategoryRequest.php
@@ -3,7 +3,6 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Validation\Rule;
 
 class CategoryRequest extends FormRequest
 {
@@ -23,12 +22,7 @@ class CategoryRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => [
-            'required',
-            'string',
-            'min:3',
-            Rule::unique('categories')->whereNull('deleted_at'),
-        ],
+            'name' => 'required|string|unique:categories|min:3',
         ];
     }
 
